443561f91834cb8d26668fa2dfd6da2a83ac2a86
gftang <gftang@vcainfo.com>
2017-06-01 17:25:34 +0800

    init

---
+++ GFPhotoBrowser/Source/GFPhotoBrowser.swift
@@ -0,0 +1,13 @@
+//
+//  GFPhotoBrowser.swift
+//  GFPhotoBrowser
+//
+//  Created by VcaiTech on 2017/6/1.
+//  Copyright © 2017年 Tang guifu. All rights reserved.
+//
+
+import UIKit
+
+class GFPhotoBrowser: NSObject {
+
+}
